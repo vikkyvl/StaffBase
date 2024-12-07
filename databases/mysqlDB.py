e0064e8112e8804597d17950805eeaf82d9ef950
vlady <vladysha1021@gmail.com>
2024-12-07 11:30:18 -0500

    Add database MySQL

--- databases/mysqlDB.py
+++ databases/mysqlDB.py
@@ -1,11 +1,12 @@
 import mysql.connector
 
 class MySQL:
-    def __init__(self, host="35.238.85.91", port=3306, user="root", password="v-7@", use_pure=True):
+    def __init__(self, host="35.238.85.91", port=3306, user="root", password="v-7@", database="staff_control", use_pure=True):
         self.mydb = mysql.connector.connect(
             host=host,
             port=port,
             user=user,
             password=password,
+            database=database,
             use_pure=use_pure
         )
