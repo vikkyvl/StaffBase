362f1d21066408ed5e5673744e8d3a17ca94b6bd
vlady <vladysha1021@gmail.com>
2024-12-12 09:11:45 -0500

    Add employee.py

--- classes/employee.py
+++ classes/employee.py
@@ -1,7 +1,16 @@
 class Employee:
-    def __init__(self, employee_id, full_name=None):
-        self._employee_id = employee_id
+    def __init__(self, employee_id: str, full_name: str = None):
+        self._employee_ID = employee_id
         self._full_name = full_name
 
-    def set_full_name(self, full_name):
+    def get_employee_ID(self) -> str:
+        return self._employee_ID
+
+    def get_full_name(self) -> str:
+        return self._full_name
+
+    def set_employee_ID(self, employee_ID: str):
+        self._employee_ID = employee_ID
+
+    def set_full_name(self, full_name: str):
         self._full_name = full_name
