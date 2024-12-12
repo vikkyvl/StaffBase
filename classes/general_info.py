d7b878ac5c94109c5a6da9fbf728e41ca601c7ee
vlady <vladysha1021@gmail.com>
2024-12-12 09:40:11 -0500

    Add general_info.py

--- classes/general_info.py
+++ classes/general_info.py
@@ -1,7 +1,37 @@
+from datetime import date
+from employee import Employee
+
 class GeneralInfo:
-    def __init__(self, employee_id, department_id, position_id, hire_date, experience):
-        self._employee_id = employee_id
-        self._department_id = department_id
-        self._position_id = position_id
+    def __init__(self, employee: Employee, department_id: int, position_id: int, hire_date: date, experience: int):
+        self._employee = employee  # Композиція з класом Employee
+        self._department_ID = department_id
+        self._position_ID = position_id
         self._hire_date = hire_date
+        self._experience = experience
+
+    def get_employee_ID(self) -> str:
+        return self._employee.get_employee_ID()
+
+    def get_department_ID(self) -> int:
+        return self._department_ID
+
+    def get_position_ID(self) -> int:
+        return self._position_ID
+
+    def get_hire_date(self) -> date:
+        return self._hire_date
+
+    def get_experience(self) -> int:
+        return self._experience
+
+    def set_department_ID(self, department_ID: int):
+        self._department_ID = department_ID
+
+    def set_position_ID(self, position_ID: int):
+        self._position_ID = position_ID
+
+    def set_hire_date(self, hire_date: date):
+        self._hire_date = hire_date
+
+    def set_experience(self, experience: int):
         self._experience = experience
