cb9c8a0f2ca0f7e36723a931ec970c5d6d632fa5
vlady <vladysha1021@gmail.com>
2024-12-14 08:25:02 -0500

    Change salary.py structure

--- classes/salary.py
+++ classes/salary.py
@@ -6,7 +6,7 @@ class Salary:
         self._month = month
         self._salary = salary
 
-    def get_employee_ID(self) -> str:
+    def get_employee_id(self) -> str:
         return self._employee.get_employee_ID()
 
     def get_month(self) -> str:
@@ -14,9 +14,6 @@ class Salary:
 
     def get_salary(self) -> float:
         return self._salary
-
-    def set_employee_ID(self, employee_ID: str):
-        self._employee.set_employee_ID(employee_ID)
 
     def set_month(self, month: str):
         self._month = month
