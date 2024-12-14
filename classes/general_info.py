375532782b6f1e5c9fa42ca44b78a75c2759eb91
vlady <vladysha1021@gmail.com>
2024-12-14 07:44:51 -0500

    Change general_info.py structure

--- classes/general_info.py
+++ classes/general_info.py
@@ -1,22 +1,28 @@
 from datetime import date
-from classes.employee import Employee
 
 class GeneralInfo:
-    def __init__(self, employee: Employee, department_id: int, position_id: int, hire_date: date, experience: int):
-        self._employee = employee
-        self._department_ID = department_id
-        self._position_ID = position_id
+    def __init__(
+        self,
+        employee_id: str,
+        department_id: int,
+        position_id: int,
+        hire_date: date,
+        experience: int
+    ):
+        self._employee_id = employee_id
+        self._department_id = department_id
+        self._position_id = position_id
         self._hire_date = hire_date
         self._experience = experience
 
-    def get_employee_ID(self) -> str:
-        return self._employee.get_employee_ID()
+    def get_employee_id(self) -> str:
+        return self._employee_id
 
-    def get_department_ID(self) -> int:
-        return self._department_ID
+    def get_department_id(self) -> int:
+        return self._department_id
 
-    def get_position_ID(self) -> int:
-        return self._position_ID
+    def get_position_id(self) -> int:
+        return self._position_id
 
     def get_hire_date(self) -> date:
         return self._hire_date
@@ -24,11 +30,11 @@ class GeneralInfo:
     def get_experience(self) -> int:
         return self._experience
 
-    def set_department_ID(self, department_ID: int):
-        self._department_ID = department_ID
+    def set_department_id(self, department_id: int):
+        self._department_id = department_id
 
-    def set_position_ID(self, position_ID: int):
-        self._position_ID = position_ID
+    def set_position_id(self, position_id: int):
+        self._position_id = position_id
 
     def set_hire_date(self, hire_date: date):
         self._hire_date = hire_date
