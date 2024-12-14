c24e4d53cd15d8e2c712739b682d94fb882ec7fc
vlady <vladysha1021@gmail.com>
2024-12-14 07:40:46 -0500

    Change company.py structure

--- classes/company.py
+++ classes/company.py
@@ -4,7 +4,6 @@ class Company:
     def __init__(self, company_name: str, num_of_departments: int):
         self._company_name = company_name
         self._num_of_departments = num_of_departments
-        self._departments = []
 
     def get_company_name(self) -> str:
         return self._company_name
@@ -18,6 +17,4 @@ class Company:
     def set_num_of_departments(self, num_of_departments: int):
         self._num_of_departments = num_of_departments
 
-    def add_department(self, department: Department):
-        self._departments.append(department)
 
