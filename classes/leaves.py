e0940184f8f2580494fe828dd93e47f848be67c0
vlady <vladysha1021@gmail.com>
2024-12-14 08:23:55 -0500

    Change leaves.py structure

--- classes/leaves.py
+++ classes/leaves.py
@@ -2,15 +2,15 @@ from datetime import date
 from classes.employee import Employee
 
 class Leaves:
-    def __init__(self, employee: Employee, leave_type: str, start_date: date, end_date: date, duration: int):
+    def __init__(self, employee: Employee, leave_type: str, start_date: date, end_date: date):
         self._employee = employee
-        self._leave_type = leave_type
+        self._leave_type = leave_type if leave_type in ('Sick', 'Vacation', 'Time Off') else None
         self._start_date = start_date
-        self._end_date = end_date
-        self._duration = duration
+        self._end_date = end_date if end_date >= start_date else start_date
+        self._duration = self._calculate_duration()
 
-    def get_employee_ID(self) -> str:
-        return self._employee.get_employee_ID()
+    def get_employee_id(self) -> str:
+        return self._employee.get_employee_id()
 
     def get_leave_type(self) -> str:
         return self._leave_type
@@ -25,13 +25,18 @@ class Leaves:
         return self._duration
 
     def set_leave_type(self, leave_type: str):
-        self._leave_type = leave_type
+        if leave_type in ('Sick', 'Vacation', 'Time Off'):
+            self._leave_type = leave_type
 
     def set_start_date(self, start_date: date):
-        self._start_date = start_date
+        if start_date <= self._end_date:
+            self._start_date = start_date
+            self._duration = self._calculate_duration()
 
     def set_end_date(self, end_date: date):
-        self._end_date = end_date
+        if end_date >= self._start_date:
+            self._end_date = end_date
+            self._duration = self._calculate_duration()
 
-    def set_duration(self, duration: int):
-        self._duration = duration
+    def _calculate_duration(self) -> int:
+        return (self._end_date - self._start_date).days + 1
