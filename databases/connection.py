dfd2d2a14040088dea93b09f54dceae565daf0f6
Victoria Leshchenko <victorialeshchenko7@gmail.com>
2024-12-16 22:49:25 +0200

    Some changes

--- databases/connection.py
+++
@@ -1,8 +0,0 @@
-# from databases.redisDB import Redis
-# from databases.mysqlDB import MySQL
-#
-# class Connection:
-#     def __init__(self):
-#         self.redis_connection = Redis()
-#         self.mysql_connection = MySQL()
-
